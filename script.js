dfb32561ab529a384fc4a97c606a4301c8770600
prasadthx <prasad.zore@outlook.com>
2021-10-14 23:15:20 +0530

    Result section added

--- script.js
+++ script.js
@@ -10,60 +10,57 @@ inputNumberField.addEventListener('change', () => {
 const startbutton = document.getElementById("startFunction");
 
 const usingJavaScript = (value) => {
-    let start = window.performance.now();
-    console.log(findPrimes(value));
-    let end = window.performance.now();
-    let time = end - start;
-    setValue(time, "JavaScriptSpeed");
+    const start = window.performance.now();
+    const result = findPrimes(value);
+    const end = window.performance.now();
+    const time = end - start;
+    setValue(`${time} ms`, "JavaScriptSpeed");
+    setValue(result, "JavaScriptResults");
 }
 
 const usingC = (value) => {
-    fetch('primeNo_C_Code.wasm').then(response =>
-        response.arrayBuffer()
-      ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
-        let instance = results.instance;
-        let start = window.performance.now();
-        Module._findPrimes(value);
-        let end = window.performance.now();
-        let time = end - start;
-        setValue(time, "CSpeed");
-      }).catch(console.error);
+    const start = window.performance.now();
+    const result = Module._findPrimes(value);
+    const end = window.performance.now();
+    const time = end - start;
+    setValue(`${time} ms`, "CSpeed");
+    setValue(result, "CResults");
 }
 
 const usingRust = (value) => {
     fetch('rustWasm.wasm').then(response =>
         response.arrayBuffer()
       ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
-        let instance = results.instance;
-        let start = window.performance.now();
-        instance.exports.find_primes(value);
-        let end = window.performance.now();
-        let time = end - start;
-        setValue(time, "RustSpeed");
+        const instance = results.instance;
+        const start = window.performance.now();
+        const result = instance.exports.find_primes(value);
+        const end = window.performance.now();
+        const time = end - start;
+        setValue(`${time} ms`, "RustSpeed");
+        setValue(result, "RustResults")
       }).catch(console.error);
 }
 
 const usingJava = (value) => {
-    let importObject = {
-        imports: { imported_func: arg => console.log(arg) }
-    };
-
     fetch('./javaCode.wasm').then(response =>
         response.arrayBuffer()
     ).then(bytes =>
-        WebAssembly.instantiate(bytes, importObject)
+        WebAssembly.instantiate(bytes)
     ).then(results => {
-        let start = window.performance.now();
-        console.log(results.instance.exports.findPrimes(value));
-        let end = window.performance.now();
-        let time = end - start;
-        setValue(time, "JavaSpeed");
+        const instance = results.instance;
+        const start = window.performance.now();
+        const result = instance.exports.findPrimes(value);
+        const end = window.performance.now();
+        const time = end - start;
+        setValue(`${time} ms`, "JavaSpeed");
+        setValue(result, "JavaResults")
       }).catch(console.error);
 }
 
 const usingSwift = async (value) => {
     const {result, time} = await executeSwiftWasm(value);
     setValue(time, "SwiftSpeed");
+    setValue(result, "SwiftResults");
 }
 
 const usingGo = (value) => {
@@ -75,18 +72,19 @@ const usingGo = (value) => {
     ).then(bytes => WebAssembly.instantiate(bytes, go.importObject)).then(results => {
       goWasm = results.instance;
       go.run(goWasm)
-      let start = window.performance.now();
-      goWasm.exports.findPrimes(value);
-      let end = window.performance.now();
-      let time = end - start;
-      setValue(time, "GoSpeed");
+      const start = window.performance.now();
+      const result = goWasm.exports.findPrimes(value);
+      const end = window.performance.now();
+      const time = end - start;
+      setValue(`${time} ms`, "GoSpeed");
+      setValue(result, "GoResults");
     }).catch(console.error);
 
 }
 
 const setValue = (value, id) => {
     let element = document.getElementById(id);
-    element.innerText = value + " ms";
+    element.innerText = value;
 } 
 
 const startButtonClicked = async () => {
--- swiftScript.js
+++ swiftScript.js
@@ -1,8 +1,5 @@
 import {WASI} from "https://cdn.skypack.dev/@wasmer/wasi";
-//import { lowerI64Imports } from "@wasmer/wasm-transformer"
 
-//import wasiBindings from "https://cdn.skypack.dev/@wasmer/wasi/lib/bindings/browser";
- 
 import { WasmFs } from "https://cdn.skypack.dev/@wasmer/wasmfs";
 
 const wasmFS = new WasmFs();
@@ -20,18 +17,16 @@ export const executeSwiftWasm = async (value) => {
   let response = await fetch('swiftOs.wasm');
   response = await response.arrayBuffer();
 
-  let { instance } = await WebAssembly.instantiate(response, {
+  const { instance } = await WebAssembly.instantiate(response, {
     wasi_snapshot_preview1: wasi.wasiImport,
   });
 
   wasi.start(instance);
-  let start = window.performance.now();
-  let result = instance.exports.find_primes(value);
-  let end = window.performance.now();
-  let time = end - start;
-  console.log("Time: " + time + " result: " + result);
+  const start = window.performance.now();
+  const result = instance.exports.find_primes(value);
+  const end = window.performance.now();
+  const time = end - start;
   returnObject.result = result
   returnObject.time = time;
-  console.log(returnObject);
-  return returnObject;
+ return returnObject;
 };
